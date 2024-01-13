d53bab50fc9a2af5c673b3edc6f8e806204be874
Jonathan Deininger <J_Deininger@web.de>
2024-01-13 02:29:18 +0100

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home';
 import Game from './game';
 import Info from './info';
